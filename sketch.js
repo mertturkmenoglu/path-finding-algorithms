b2fbf075c425a569fa184d7033f07cc17af12c2a
Mert <mertturkmenoglu99@gmail.com>
2019-07-26 03:25:44 +0300

    Add key press detection

--- sketch.js
+++ sketch.js
@@ -4,13 +4,14 @@ const sRadius = 50  // square radius
 const rowCount = Math.round(canvasHeight / sRadius)
 const colCount = Math.round(canvasWidth / sRadius)
 
-let board = [];
-let mouseClickCount = 0;
+let board = []
+let mouseClickCount = 0
+let completed = false
 
 function setup() {
-    createCanvas(canvasHeight, canvasWidth);
+    createCanvas(canvasHeight, canvasWidth)
     for (let i = 0; i < rowCount; i++) {
-        board[i] = [];
+        board[i] = []
         for (let j = 0; j < colCount; j++) {
             board[i][j] = 0
         }
@@ -18,45 +19,49 @@ function setup() {
 }
 
 function draw() {
-    background(255);
-    drawShapes();
-    drawGrid();
+    background(255)
+    drawShapes()
+    drawGrid()
+}
+
+function keyPressed() {
+    if (key === 's') {
+        console.log("S pressed")
+    }
 }
 
 function mouseClicked() {
-    console.log("clicked");
-    let coord = getCoordinates(mouseX, mouseY);
+    let coord = getCoordinates(mouseX, mouseY)
+    
     if (mouseClickCount == 0) {
         board[coord[1]][coord[0]] = 1
-        mouseClickCount++
     } else if (mouseClickCount == 1) {
         board[coord[1]][coord[0]] = 2
-        mouseClickCount++
     } else {
         board[coord[1]][coord[0]] = 3
-        mouseClickCount++
     }
-    console.log(coord);
+
+    mouseClickCount++
 }
 
 function getCoordinates(xpos, ypos) {
-    xpos /= sRadius;
-    ypos /= sRadius;
-    return [int(ypos), int(xpos)];
+    xpos /= sRadius
+    ypos /= sRadius
+    return [int(ypos), int(xpos)]
 }
 
 function drawGrid() {
     fill(0)
     for (let i = 0; i <= width; i += sRadius) {
-        line(i, 0, i, height);
-        line(0, i, width, i);
+        line(i, 0, i, height)
+        line(0, i, width, i)
     }
 }
 
 function drawShapes() {
     fill(255)
-    rectMode(CENTER);
-    ellipseMode(CENTER);
+    rectMode(CENTER)
+    ellipseMode(CENTER)
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
