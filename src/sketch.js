dad3f59bede28d00b9839e793898014518ca728a
Mert <mertturkmenoglu99@gmail.com>
2020-01-15 01:39:52 +0300

    Update UI and user interaction
    - Change canvas and objects.
    - Add mouseDragged event listener
    - Prevent default right click event

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,19 @@
 let board;
-const w = 960;
-const h = 640;
 const r = 32;
-const rowCount = 20;
-const colCount = 30;
+let rowCount;
+let colCount;
 let statePoints;
 let path;
 let gameStatus;
 
 function setup() {
-    createCanvas(960, 640);
+    createCanvas(windowWidth, windowHeight - 16);
+
+    rowCount = Math.floor((windowHeight - 16) / r);
+    colCount = parseInt(windowWidth / r);
+
+    console.log("Width")
+    console.log(innerWidth)
     board = new Array(rowCount);
     for (let i = 0; i < rowCount; i++) {
         board[i] = new Array(colCount).fill(0);
@@ -18,6 +22,9 @@ function setup() {
     statePoints = [0, 0];
     gameStatus = 0; // -1: No path 0: Continue 1: Path
     textAlign(CENTER, CENTER);
+
+    document.addEventListener('contextmenu', e => e.preventDefault());
+    alert("Left click to add an object, right click to remove an object.\nPress 'S' to start path finding.")
 }
 
 function draw() {
@@ -49,6 +56,7 @@ function draw() {
 }
 
 function drawTable() {
+    stroke(150);
     for (let i = 0; i <= rowCount; i++) {
         line(0, r * i, width, r * i);
     }
@@ -63,16 +71,16 @@ function drawBoard() {
         for (let j = 0; j < colCount; j++) {
             let value = board[i][j];
             if (value === 1) {
-                fill(0, 255, 0);
+                fill(188, 16, 222);
                 ellipse(j*r+(r/2), i*r+(r/2), r, r);
             } else if (value === 2) {
-                fill(0);
-                rect(j*r, i*r, r, r);
+                fill(100);
+                rect(j*r, i*r, r-1, r-1);
             } else if (value === 3) {
-                fill(255, 0, 0);
+                fill(222, 16, 50);
                 ellipse(j*r+(r/2), i*r+(r/2), r, r);
             } else if (value === 4) {
-                fill(0, 0, 255);
+                fill(228, 186, 34);
                 ellipse(j*r+(r/2), i*r+(r/2), r, r);
             }
         }
@@ -236,4 +244,11 @@ function mousePressed() {
             board[i][j] = 0;
         }
     }
+
+    return false
+}
+
+function mouseDragged() {
+    mousePressed();
+    return false;
 }
