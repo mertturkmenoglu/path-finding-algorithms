ef6319e6137229923f4824983bc25d48df43f2c3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-22 13:15:41 +0300

    update Pos lib

--- lib/Pos.ts
+++ lib/Pos.ts
@@ -1,7 +1,7 @@
 export type Pos = [number, number];
 
 export function posEq(a: Pos, b: Pos): boolean {
-  return JSON.stringify(a) === JSON.stringify(b);
+  return a[0] === b[0] && a[1] === b[1];
 }
 
 export function posDiff(a: Pos, b: Pos): [number, number] {
