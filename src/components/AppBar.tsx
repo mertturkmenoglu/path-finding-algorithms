ee55c11689288fe44f3fdca57d1d1644e3d6166b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-23 07:44:57 +0300

    add maze generation

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -7,8 +7,19 @@ import {
   SelectValue,
 } from './Select';
 import { AppContext } from '../contexts/AppContext';
-import { TAlgorithm, THeuristic } from '../../lib/GraphNode';
-import { astar, bfs } from '../../lib/algorithms';
+import {
+  BfsNode,
+  GraphNode,
+  TAlgorithm,
+  THeuristic,
+} from '../../lib/GraphNode';
+import {
+  PathFindingResult,
+  astar,
+  bfs,
+  dfs,
+  dijkstra,
+} from '../../lib/algorithms';
 import { posEq } from '../../lib/Pos';
 
 function AppBar(): React.ReactElement {
@@ -17,10 +28,26 @@ function AppBar(): React.ReactElement {
   const start = () => {
     ctx.g.clearPathAndVisited();
 
-    const res =
-      ctx.algorithm === 'astar'
-        ? astar(ctx.g, ctx.start!, ctx.end!, 'manhattan', 'astar')
-        : bfs(ctx.g, ctx.start!, ctx.end!);
+    if (ctx.start === null) {
+      alert('Select a start point');
+      return;
+    }
+
+    if (ctx.end === null) {
+      alert('Select an end point');
+      return;
+    }
+
+    let res: PathFindingResult<GraphNode> | PathFindingResult<BfsNode> | null =
+      null;
+
+    if (ctx.algorithm === 'astar') {
+      res = astar(ctx.g, ctx.start, ctx.end, ctx.heuristic, 'astar');
+    } else if (ctx.algorithm === 'dijkstra') {
+      res = dijkstra(ctx.g, ctx.start, ctx.end);
+    } else {
+      res = bfs(ctx.g, ctx.start, ctx.end);
+    }
 
     if (res.path.length === 0) {
       alert('Cannot find a path');
@@ -56,6 +83,12 @@ function AppBar(): React.ReactElement {
 
   const clear = () => {
     ctx.g.clearPathAndVisited();
+    ctx.trigger();
+  };
+
+  const maze = () => {
+    reset();
+    dfs(ctx.g);
     ctx.trigger();
   };
 
@@ -102,7 +135,7 @@ function AppBar(): React.ReactElement {
       </div>
 
       <div className="flex items-center space-x-4">
-        <button>Generate Maze</button>
+        <button onClick={maze}>Generate Maze</button>
         <button onClick={reset}>Reset</button>
         <button onClick={clear}>Clear</button>
         <button onClick={start}>Start</button>
