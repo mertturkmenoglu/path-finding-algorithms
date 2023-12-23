14a09cc80a5e4696f33446a3ad2368ea29967ed3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-23 06:42:11 +0300

    update algorithms

--- lib/algorithms.ts
+++ lib/algorithms.ts
@@ -1,7 +1,11 @@
 import { BfsNode, GraphNode, TAlgorithm, THeuristic } from './GraphNode';
 import { Grid } from './Grid';
 import { Pos, posEq } from './Pos';
-import Heap from 'heap';
+
+interface PathFindingResult<T> {
+  path: T[];
+  visited: T[];
+}
 
 function constructPath<T extends { parent: T | null }>(child: T): T[] {
   const path: T[] = [];
@@ -23,23 +27,26 @@ export function astar(
   end: Pos,
   heuristic: THeuristic,
   algorithm: TAlgorithm = 'astar'
-): GraphNode[] {
-  const open = new Heap<GraphNode>((a, b) => a.f - b.f);
+): PathFindingResult<GraphNode> {
+  const open: GraphNode[] = [];
   const closed: GraphNode[] = [];
   const startNode = new GraphNode(algorithm, heuristic, null, start);
   const endNode = new GraphNode(algorithm, heuristic, null, end);
 
   open.push(startNode);
 
-  while (!open.empty()) {
-    const q = open.pop()!;
+  while (open.length > 0) {
+    const q = open.shift()!;
     const children = g
       .getCardinalNeighboursPos(q.pos)
-      .map((p) => new GraphNode('astar', heuristic, q, p));
+      .map((p) => new GraphNode(algorithm, heuristic, q, p));
 
     for (const successor of children) {
       if (posEq(successor.pos, end)) {
-        return constructPath(successor);
+        return {
+          path: constructPath(successor),
+          visited: closed,
+        };
       }
 
       successor.updateValues(endNode);
@@ -48,27 +55,34 @@ export function astar(
         posEq(n.pos, successor.pos) && n.f < successor.f;
 
       if (
-        open.toArray().some(lowerF) ||
+        open.some(lowerF) ||
         closed.some(lowerF) ||
         closed.some((n) => posEq(n.pos, successor.pos))
       ) {
         continue;
+      } else {
+        closed.push(successor);
+        open.push(successor);
+        open.sort((a, b) => a.f - b.f);
       }
-
-      open.push(successor);
     }
-
-    closed.push(q);
   }
 
-  return [];
+  return {
+    path: [],
+    visited: closed,
+  };
 }
 
-export function dijkstra(g: Grid, start: Pos, end: Pos): GraphNode[] {
+export function dijkstra(
+  g: Grid,
+  start: Pos,
+  end: Pos
+): PathFindingResult<GraphNode> {
   return astar(g, start, end, 'euclidean', 'dijkstra');
 }
 
-export function bfs(g: Grid, start: Pos, end: Pos): BfsNode[] {
+export function bfs(g: Grid, start: Pos, end: Pos): PathFindingResult<BfsNode> {
   const startNode = new BfsNode(null, start);
   const endNode = new BfsNode(null, end);
   const open: BfsNode[] = [startNode];
@@ -82,7 +96,10 @@ export function bfs(g: Grid, start: Pos, end: Pos): BfsNode[] {
 
     for (const child of children) {
       if (child.isEqual(endNode)) {
-        return constructPath(child);
+        return {
+          path: constructPath(child),
+          visited: closed,
+        };
       }
 
       if (closed.some((n) => posEq(n.pos, child.pos))) {
@@ -94,5 +111,8 @@ export function bfs(g: Grid, start: Pos, end: Pos): BfsNode[] {
     }
   }
 
-  return [];
+  return {
+    path: [],
+    visited: closed,
+  };
 }
